5c4a41e21a4f74cd16d31a29cc42a4077dcaca5d
adsoftsito <adsoft@live.com.mx>
2020-11-14 02:24:36 -0600

    add excel-simpson rule

--- w13/integracion/simpson_1.m
+++ w13/integracion/simpson_1.m
@@ -1,30 +1,35 @@
 clc;
 clear all;
 % f=@(x)cos(x)-log(x)+exp(x); %Change here for different function
+
 %f=@(x) 2*(x); %Change here for different function
+%f=@(x) (1/x);
 
 dof = 9
 
 % f=@(x) (1+((x^2)/dof))^(-1*(dof+1)/2) ; %Change here for different function
-% f=@(x) (gamma((dof+1)/2)) / (( (dof * 3.1416) ^ 0.5 )*( gamma(dof/2))); %Change here for different function
+%f=@(x) (gamma((dof+1)/2)) / (( (dof * 3.1416) ^ 0.5 )*( gamma(dof/2))); %Change here for different function
 
  %Change here for different function
 f=@(x) ( 1+((x^2)/dof))^(-1*(dof+1)/2) * (gamma((dof+1)/2)) / (( (dof * pi) ^ (1/2) )*( gamma(dof/2))); 
 
 %Change here for different function
-%f=@(x) exp(x^2)
+%f=@(x) (x * x);
 %f=@(x) (1/x)
 
 %f=@(x) (1/x)
 
 a=input('Enter lower limit a: '); % exmple a=1
- b=input('Enter upper limit b: ');  % exmple b=2
- n=input('Enter the  number of sub-intervals n: '); % exmple n=16
- h=(b-a)/n;
+b=input('Enter upper limit b: ');  % exmple b=2
+
+n=input('Enter the  number of sub-intervals n: '); % exmple n=16
+h=(b-a)/n;
+
 if rem(n,2)==1
    fprintf('\n Enter valid n!!!'); 
    n=input('\n Enter n as even number ');
 end
+
 for k=1:1:n
   x(k)=a+k*h;
   y(k)=f(x(k));
